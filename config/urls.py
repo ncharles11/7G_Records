ed5c0ccc562aa45a43369af65016ae6ad5a8d3ef
ncharles11 <123888602+ncharles11@users.noreply.github.com>
2025-08-25 20:10:02 +0200

    Add files via upload

---
+++ config/urls.py
@@ -0,0 +1,15 @@
+from django.contrib import admin
+from django.urls import path
+from music import views
+from django.conf import settings
+from django.conf.urls.static import static
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('artists/', views.artists, name='artists'),
+    path('music/', views.music, name='music'),
+    path('about/', views.about, name= 'about'),
+]
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
